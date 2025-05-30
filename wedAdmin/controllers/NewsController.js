752fa1126d0721edd2eb202e444174f914e889c1
QuangGiap73 <voquanggiap73@gmail.com>
2025-05-30 13:40:33 +0700

    up

--- wedAdmin/controllers/NewsController.js
+++ wedAdmin/controllers/NewsController.js
@@ -18,3 +18,45 @@ exports.store = async (req, res) => {
   await db.collection('news').add({ titles, contents, Img, createAt: new Date() });
   res.redirect('/news');
 };
+
+// chỉnh sửa
+exports.edit = async (req, res) => {
+  const id = req.params.id;
+  const doc = await db.collection('news').doc(id).get();
+
+  if (!doc.exists) {
+    return res.status(404).send('Không tìm thấy bài viết');
+  }
+
+  res.render('news/edit', {
+    newsItem: { id: doc.id,...doc.data()}
+  });
+};
+// update
+exports.update = async (req, res) => {
+  const id = req.params.id;
+  const {titles,contents} = req.body;
+  const Img = req.file?.path || null;
+
+
+  const dataUpdate = {titles,contents};
+  if(Img) dataUpdate.Img = Img;
+
+  await db.collection('news').doc(id).update(dataUpdate);
+  res.redirect('/news');
+}
+// xoa bai viet
+exports.destroy = async (req, res) => {
+  const id = req.params.id;
+  await db.collection('news').doc(id).delete();
+  res.redirect('/news');
+};
+
+// xem chi tiết bài viết 
+exports.view = async (req, res) => {
+  const id = req.params.id;
+  const doc = await db.collection('news').doc(id).get();
+  if (!doc.exists) return res.status(404).send('Không tìm thấy bài viết');
+  const data = doc.data();
+  res.render('news/view', { newsItem: { id, ...data } });
+};
--- wedAdmin/routes/news.js
+++ wedAdmin/routes/news.js
@@ -7,4 +7,10 @@ router.get('/', newsController.index);
 router.get('/create', newsController.create);
 router.post('/store', upload.single('image'), newsController.store);
 
+router.get('/edit/:id', newsController.edit);
+router.post('/delete/:id', newsController.destroy);
+router.post('/update/:id', upload.single('image'), newsController.update);
+
+router.get('/view/:id', newsController.view);
+
 module.exports = router;
