b1713d262d199744395186537d0123003ad30c14
auroratravelvn <22010338@st.phenikaa-uni.edu.vn>
2025-06-24 22:02:45 +0700

    hguy

--- dat_ve_xe/lib/widgets/netwwork_status_banner.dart
+++ dat_ve_xe/lib/widgets/netwwork_status_banner.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:dat_ve_xe/service/connectivity_service.dart'; // thay bằng path của bạn
+import 'package:dat_ve_xe/service/connectivity_service.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class NetworkStatusBanner extends StatelessWidget {
   const NetworkStatusBanner({super.key});
@@ -9,14 +10,14 @@ class NetworkStatusBanner extends StatelessWidget {
   Widget build(BuildContext context) {
     final connectivityService = Provider.of<ConnectivityService>(context);
     if (connectivityService.isConnected) return const SizedBox.shrink();
-
+    final t = AppLocalizations.of(context)!;
     return AlertDialog(
       title: const Text("Không có kết nối mạng"),
       content: const Text("Vui lòng kiểm tra kết nối mạng của bạn."),
       actions: [
         TextButton(
           onPressed: () => connectivityService.retry(),
-          child: const Text("Thử lại"),
+          child: Text(t.retry),
         ),
       ],
     );
