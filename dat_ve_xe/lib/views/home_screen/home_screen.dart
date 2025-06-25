a3ef8af114fbe333c039157d65e4814e33fe7b53
Nguyễn Trần Việt Anh <vietanhnguyen32004@gmail.com>
2025-06-26 01:34:30 +0700

    Update home_screen.dart

--- dat_ve_xe/lib/views/home_screen/home_screen.dart
+++ dat_ve_xe/lib/views/home_screen/home_screen.dart
@@ -38,90 +38,76 @@ class _HomeScreenState extends State<HomeScreen> {
       context: context,
       isScrollControlled: true,
       backgroundColor: Colors.transparent,
-      builder:
-          (context) => Container(
-            height: MediaQuery.of(context).size.height * 0.7,
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-            ),
-            child: Column(
-              children: [
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: const BoxDecoration(
-                    color: Color.fromARGB(255, 253, 109, 37),
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(20),
+      builder: (context) => Container(
+        height: MediaQuery.of(context).size.height * 0.7,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(16),
+              decoration: const BoxDecoration(
+                color: Color.fromARGB(255, 253, 109, 37),
+                borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    isStart ? 'Chọn điểm đi' : 'Chọn điểm đến',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
                     ),
                   ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        isStart ? 'Chọn điểm đi' : 'Chọn điểm đến',
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.close, color: Colors.white),
-                        onPressed: () => Navigator.pop(context),
-                      ),
-                    ],
+                  IconButton(
+                    icon: const Icon(Icons.close, color: Colors.white),
+                    onPressed: () => Navigator.pop(context),
                   ),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: _locations.length,
-                    itemBuilder: (context, index) {
-                      final location = _locations[index];
-                      final isSelected =
-                          isStart
-                              ? location == _selectedStart
-                              : location == _selectedDestination;
-
-                      return ListTile(
-                        title: Text(
-                          location,
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight:
-                                isSelected
-                                    ? FontWeight.bold
-                                    : FontWeight.normal,
-                            color:
-                                isSelected
-                                    ? Color.fromARGB(255, 253, 109, 37)
-                                    : Colors.black,
-                          ),
-                        ),
-                        trailing:
-                            isSelected
-                                ? const Icon(
-                                  Icons.check,
-                                  color: Color.fromARGB(255, 253, 109, 37),
-                                )
-                                : null,
-                        onTap: () {
-                          setState(() {
-                            if (isStart) {
-                              _selectedStart = location;
-                            } else {
-                              _selectedDestination = location;
-                            }
-                          });
-                          Navigator.pop(context);
-                        },
-                      );
-                    },
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _locations.length,
+                itemBuilder: (context, index) {
+                  final location = _locations[index];
+                  final isSelected = isStart 
+                      ? location == _selectedStart 
+                      : location == _selectedDestination;
+                  
+                  return ListTile(
+                    title: Text(
+                      location,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+                        color: isSelected ? Color.fromARGB(255, 253, 109, 37) : Colors.black,
+                      ),
+                    ),
+                    trailing: isSelected 
+                        ? const Icon(Icons.check, color: Color.fromARGB(255, 253, 109, 37))
+                        : null,
+                    onTap: () {
+                      setState(() {
+                        if (isStart) {
+                          _selectedStart = location;
+                        } else {
+                          _selectedDestination = location;
+                        }
+                      });
+                      Navigator.pop(context);
+                    },
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -131,32 +117,32 @@ class _HomeScreenState extends State<HomeScreen> {
 
     return Scaffold(
       backgroundColor: Colors.white,
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Chào bạn',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 25, // to hơn
-                fontWeight: FontWeight.bold,
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          title: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Chào bạn',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 25, // to hơn
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-            ),
-            SizedBox(height: 1),
-            Text(
-              'Đã có vé về quê chưa',
-              style: TextStyle(
-                color: const Color.fromARGB(255, 39, 38, 38),
-                fontSize: 16, // nhỏ hơn
+              SizedBox(height: 1),
+              Text(
+                'Đã có vé về quê chưa',
+                style: TextStyle(
+                  color: const Color.fromARGB(255, 39, 38, 38),
+                  fontSize: 16, // nhỏ hơn
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
+          iconTheme: IconThemeData(color: Color.fromARGB(255, 253, 109, 37)),
         ),
-        iconTheme: IconThemeData(color: Color.fromARGB(255, 253, 109, 37)),
-      ),
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Column(
@@ -164,10 +150,7 @@ class _HomeScreenState extends State<HomeScreen> {
             // Hộp chọn điểm đi & đến
             Container(
               decoration: BoxDecoration(
-                border: Border.all(
-                  color: Color.fromARGB(255, 253, 109, 37),
-                  width: 2,
-                ),
+                border: Border.all(color: Color.fromARGB(255, 253, 109, 37), width: 2),
                 borderRadius: BorderRadius.circular(12),
                 color: Colors.white,
                 boxShadow: [
@@ -185,10 +168,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     onTap: () => _showLocationPicker(true),
                     child: Row(
                       children: [
-                        Icon(
-                          Icons.radio_button_checked,
-                          color: Color.fromARGB(255, 253, 109, 37),
-                        ),
+                        Icon(Icons.radio_button_checked, color: Color.fromARGB(255, 253, 109, 37)),
                         SizedBox(width: 10),
                         Expanded(
                           child: Text(
@@ -199,10 +179,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                           ),
                         ),
-                        Icon(
-                          Icons.arrow_drop_down,
-                          color: Color.fromARGB(255, 253, 109, 37),
-                        ),
+                        Icon(Icons.arrow_drop_down, color: Color.fromARGB(255, 253, 109, 37)),
                       ],
                     ),
                   ),
@@ -211,10 +188,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     onTap: () => _showLocationPicker(false),
                     child: Row(
                       children: [
-                        Icon(
-                          Icons.location_on,
-                          color: Color.fromARGB(255, 253, 109, 37),
-                        ),
+                        Icon(Icons.location_on, color: Color.fromARGB(255, 253, 109, 37)),
                         SizedBox(width: 10),
                         Expanded(
                           child: Text(
@@ -225,10 +199,7 @@ class _HomeScreenState extends State<HomeScreen> {
                             ),
                           ),
                         ),
-                        Icon(
-                          Icons.arrow_drop_down,
-                          color: Color.fromARGB(255, 253, 109, 37),
-                        ),
+                        Icon(Icons.arrow_drop_down, color: Color.fromARGB(255, 253, 109, 37)),
                       ],
                     ),
                   ),
@@ -239,10 +210,7 @@ class _HomeScreenState extends State<HomeScreen> {
             // Hộp chọn ngày
             Container(
               decoration: BoxDecoration(
-                border: Border.all(
-                  color: Color.fromARGB(255, 253, 109, 37),
-                  width: 2,
-                ),
+                border: Border.all(color: Color.fromARGB(255, 253, 109, 37), width: 2),
                 borderRadius: BorderRadius.circular(12),
                 color: Colors.white,
                 boxShadow: [
@@ -256,29 +224,19 @@ class _HomeScreenState extends State<HomeScreen> {
               padding: EdgeInsets.symmetric(horizontal: 16, vertical: 12),
               child: Row(
                 children: [
-                  Icon(
-                    Icons.calendar_today,
-                    color: Color.fromARGB(255, 253, 109, 37),
-                  ),
+                  Icon(Icons.calendar_today, color: Color.fromARGB(255, 253, 109, 37)),
                   SizedBox(width: 10),
                   Expanded(
                     child: Text(
                       _selectedDate != null
                           ? 'Ngày đi: ${dateFormat.format(_selectedDate!)}'
                           : 'Chưa chọn ngày đi',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 253, 109, 37),
-                      ),
+                      style: TextStyle(color: Color.fromARGB(255, 253, 109, 37)),
                     ),
                   ),
                   TextButton(
                     onPressed: _pickDate,
-                    child: Text(
-                      'Chọn ngày',
-                      style: TextStyle(
-                        color: Color.fromARGB(255, 253, 109, 37),
-                      ),
-                    ),
+                    child: Text('Chọn ngày', style: TextStyle(color: Color.fromARGB(255, 253, 109, 37))),
                   ),
                 ],
               ),
@@ -297,9 +255,7 @@ class _HomeScreenState extends State<HomeScreen> {
               onPressed: () {
                 if (_selectedStart == _selectedDestination) {
                   ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      content: Text('Điểm đi và đến không được trùng nhau'),
-                    ),
+                    SnackBar(content: Text('Điểm đi và đến không được trùng nhau')),
                   );
                   return;
                 }
@@ -315,14 +271,13 @@ class _HomeScreenState extends State<HomeScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder:
-                        (_) => SearchResultScreen(
-                          startLocation: _selectedStart!,
-                          destination: _selectedDestination!,
-                          selectedDate: _selectedDate!,
-                          searchByStopsStart: true,
-                          searchByStopsEnd: true,
-                        ),
+                    builder: (_) => SearchResultScreen(
+                      startLocation: _selectedStart!,
+                      destination: _selectedDestination!,
+                      selectedDate: _selectedDate!,
+                      searchByStopsStart: true,
+                      searchByStopsEnd: true,
+                    ),
                   ),
                 );
               },
